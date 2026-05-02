d4f57b5bf066d2af1ecbabedc8db65fa601b2151
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-02 18:50:23 +0530

    0 ms time complexity

---
+++ Array/6.Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,15 @@
+def removeDuplicates(nums):
+        l=0
+        r=1
+       
+        for i in range(len(nums)-1):
+            if nums[r] == nums[l]:
+                r+=1
+            else:
+                nums[l+1] = nums[r]
+                l+=1
+                r+=1
+        return l+1
+
+nums = [0,0,1,1,1,2,2,3,3,3,4]
+print(removeDuplicates(nums))
