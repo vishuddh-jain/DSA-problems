8f7a0ec182e6dcb9f1f83c6a7e0be4a9bcf781df
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-03 23:29:34 +0530

    add greedy approch for jump game 2

--- 2.Greedy & DP Array/2.Jump_Game_II.py
+++ 2.Greedy & DP Array/2.Jump_Game_II.py
@@ -15,3 +15,23 @@ def minJumps(nums, i=0):
     return min_steps
 nums = [2,3,1,1,4]
 print(minJumps(nums, i =0))
+
+# greedy approach 
+
+def jump(nums):
+    jumps = 0
+    current_end = 0
+    farthest = 0
+
+    for i in range(len(nums) - 1):
+        farthest = max(farthest, i + nums[i])
+
+        # when we reach end of current range
+        if i == current_end:
+            jumps += 1
+            current_end = farthest
+
+    return jumps
+
+nums = [2,3,1,1,4]
+print(jump(nums))
