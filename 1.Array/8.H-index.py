4191b236e3b279f4310244a4a7e5b80a4b7d7327
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-04 15:08:30 +0530

    added brute force approach with 402 ms time complexity

--- 1.Array/8.H-index.py
+++ 1.Array/8.H-index.py
@@ -1,12 +1,29 @@
-def h_index(nums):
-    arr = sorted(nums)
+# Using inbuilt function sorted()
+
+def h_index(citations):
+    arr = sorted(citations)
     sort_arr = arr[::-1]
     count = 0
     for i in range(len(sort_arr)):
         if sort_arr[i] >= i+1:
-            print(sort_arr[i])
             count+=1
     return count
         
 print(h_index([3,0,6,1,2,4,5]))
 
+
+# Brute force approach
+
+def h_index(citations):
+    n = len(citations)
+    for h in range(n,-1,-1): # we are looping backwards so that we get the h that occured atleast h times
+        count = 0
+        
+        for c in citations:
+            if c >= h:
+                count+=1
+
+            if count >= h:
+                return count
+            
+print(h_index([3,0,6,1,2,4,5,7,8,9]))
