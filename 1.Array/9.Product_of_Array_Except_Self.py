5c74a5a7408965874b2eaa3e41fa641f2d29e71b
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-04 23:35:02 +0530

    adding 17 ms time complexity approach O(n)

--- 1.Array/9.Product_of_Array_Except_Self.py
+++ 1.Array/9.Product_of_Array_Except_Self.py
@@ -1,6 +1,6 @@
 # basic approach time complexity O(n^2)
-
 import math
+
 def productExceptSelf(nums):
     answer = []
 
@@ -12,3 +12,27 @@ def productExceptSelf(nums):
 
 nums = [1,2,3,4]
 print(productExceptSelf(nums))
+
+
+# using optimized approach to get O(n) time complexity
+
+def productExceptSelf(nums):
+    n = len(nums)
+    answer = [1]*n
+
+    prefix =1
+    for i in range(n):
+        answer[i] = prefix
+        prefix *= nums[i]
+    
+    suffix =1
+    for i in range(n-1, -1,-1):
+        answer[i] *= suffix
+        suffix *=nums[i]
+
+    return answer
+
+nums = [1,2,3,4]
+print(productExceptSelf(nums))
+
+
