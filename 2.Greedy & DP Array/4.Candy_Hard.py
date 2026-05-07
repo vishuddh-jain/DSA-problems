4b76f38f8a82aa25d47ca8de74f5996cd81ab117
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-07 15:09:17 +0530

    17 ms time complexity, Added optimized version

--- 2.Greedy & DP Array/4.Candy_Hard.py
+++ 2.Greedy & DP Array/4.Candy_Hard.py
@@ -1,6 +1,6 @@
 # my Approach Not working fine for diff test cases 
 
-def Candy(ratings):
+def candy(ratings):
     ratings.append(0)
     candy_sum = 0
     for student in range(len(ratings)-2):
@@ -12,4 +12,25 @@ def Candy(ratings):
             candy_sum +=1
     return candy_sum
 ratings = [1,2,2]
+print(candy(ratings))
+
+# optimized approach
+
+def Candy(ratings):
+    n = len(ratings)
+    candies = [1]*n
+
+    # Left to right comparison
+    for i in range(1,n):
+        if ratings[i] > ratings[i-1]:
+            candies[i] = candies[i-1] +1
+
+    # Right to left comparison
+    for i in range(n-2,-1,-1): # to start loop with middle element
+        if ratings[i] >ratings[i+1]:
+            candies[i] = max(candies[i], candies[i+1]+1)
+    
+    return sum(candies)
+
+ratings = [1,0,2]
 print(Candy(ratings))
