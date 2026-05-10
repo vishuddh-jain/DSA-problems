c19ad571016109e6bded5401bc788a9e89e19c21
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-10 14:22:19 +0530

    time complexity 7 ms, string approach

---
+++ 3.String/3.Int_to_Roman.py
@@ -0,0 +1,29 @@
+def intToRoman(num):
+    my_dict = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")
+    ]
+
+    result = ""
+
+    for value, symbol in my_dict:
+
+        while num >= value:
+            result+=(symbol)
+            num -= value
+
+    return (result)
+
+num = 490
+print(intToRoman(num))
