5159e2896c27fc95156a2b75223561a385d195c0
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-22 23:41:03 +0530

    added question description

--- GFG/Linked List/Middle_of_LL.py
+++ GFG/Linked List/Middle_of_LL.py
@@ -1,3 +1,22 @@
+'''
+Given the head of singly linked list, find middle node of the linked list.
+
+If the number of nodes is odd, return the middle node.
+If the number of nodes is even, there are two middle nodes, so return the second middle node.
+Example:
+
+input:
+head -> 1 -> 2 -> 3 -> 4 -> 5 -> Null 
+ 
+Output: 3 
+Explanation: There are 5 nodes in the linked list and there is one middle node whose value is 3.
+
+Input:
+head -> 10 -> 20 -> 30 -> 40 -> 50 -> 60 -> Null 
+ 
+Output: 40
+Explanation: There are 6 nodes in the linked list, so we have two middle nodes: 30 and 40, but we will return the second middle node which is 40.
+'''
 
 class node:
     def __init__(self,data):
