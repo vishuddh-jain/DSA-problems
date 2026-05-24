74c4270c7b8bbbe4a8d9b84ee6a9a4f22c0a0a01
Vishuddh Jain <vishuddh.study@gmail.com>
2026-05-24 20:34:40 +0530

    Added question

--- GFG/Linked List/4.check_if_circular.py
+++ GFG/Linked List/4.check_if_circular.py
@@ -1,3 +1,20 @@
+'''
+Given the head of a singly linked list, the task is to find if given linked list is circular or not. A linked list is called circular if its last node points back to its first node.
+
+Note: The linked list does not contain any internal loops.
+
+Example:
+
+Input: LinkedList: 2->4->6->7->5
+Output: true
+Explanation: As shown in figure the first and last node is connected, i.e. 5 -> 2
+
+Input: LinkedList: 2->4->6->7->5->1
+Output: false
+Explanation: As shown in figure this is not a circular linked list.
+'''
+
+
 class Node:
     def __init__(self, data):
         self.data = data
