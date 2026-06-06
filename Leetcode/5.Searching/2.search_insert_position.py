c5c99169415e92ef22dabb8a3e455e24e8fb6255
Vishuddh Jain <vishuddh.study@gmail.com>
2026-06-06 14:47:12 +0530

    Time Complexity 0ms

---
+++ Leetcode/5.Searching/2.search_insert_position.py
@@ -0,0 +1,35 @@
+'''
+Given a sorted array of distinct integers and a target value,
+return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+'''
+
+def searchInsert( nums, target):
+        low = 0
+        high = len(nums) - 1
+        if target > nums[high]:
+            return (high+1)
+        if target < nums[low]:
+            return low
+        while low < high:
+            mid = low + (high-low)//2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                high = mid
+            else:
+                low = mid + 1
+        return low
