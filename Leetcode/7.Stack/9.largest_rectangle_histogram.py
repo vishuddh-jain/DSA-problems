155eed6f3d4b7699c3317a0d15ad72a39b30c3db
Vishuddh Jain <vishuddh.study@gmail.com>
2026-06-16 23:08:58 +0530

    time complexity 125ms

--- Leetcode/7.Stack/9.largest_rectangle_histogram.py
+++ Leetcode/7.Stack/9.largest_rectangle_histogram.py
@@ -15,22 +15,21 @@ def largestRectangleArea(heights):
         
         stack = []
         max_area = 0
-        n = len(heights)
 
-        for i in range(n + 1):
+        for i in range(len(heights)+1):
 
-            current_height = 0 if i == n else heights[i]
+            current_height = 0 if i == len(heights) else heights[i]
 
             while stack and current_height < heights[stack[-1]]:
 
-                h = heights[stack.pop()]
+                left = stack.pop()
 
                 if not stack:
                     width = i
                 else:
                     width = i - stack[-1] - 1
 
-                max_area = max(max_area, h * width)
+                max_area = max(max_area, heights[left] * width)
 
             stack.append(i)
 
