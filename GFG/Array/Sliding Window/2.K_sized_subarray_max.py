b0bdf040757752e383c577321d43dbc233fff5cc
Vishuddh Jain <vishuddh.study@gmail.com>
2026-07-06 15:57:55 +0530

    added Deque approach with time complexity 1.24ms

--- GFG/Array/Sliding Window/2.K_sized_subarray_max.py
+++ GFG/Array/Sliding Window/2.K_sized_subarray_max.py
@@ -19,6 +19,7 @@ Input: arr[] = [5, 1, 3, 4, 2], k = 1
 Output: [5, 1, 3, 4, 2]
 Explanation: When k = 1, each element in the array is its own subarray, so the output is simply the same array
 '''
+# Time Limit Exceeding Approach
 
 def maxOfSubarray(arr, k):
     l = 0
@@ -31,3 +32,29 @@ def maxOfSubarray(arr, k):
     
 arr = [1, 2, 3, 1, 4, 5, 2, 3, 6]
 print(maxOfSubarray(arr, 3))
+
+# Deque approach
+
+from collections import deque
+def maxOfSubarrays(arr, k):
+        result =[]
+        dq = deque()
+        for i in range(len(arr)):
+            
+            # confirming if element not inside the window, if outside -> remove it
+            while dq and dq[0] <= i-k:
+                dq.popleft()
+                
+            # confirming if element is smaller than current and removing it
+            while dq and arr[dq[-1]] <= arr[i]:
+                dq.pop()
+                
+            dq.append(i)
+            
+            # confirming window is formed of size K 
+            if i >= k-1:
+                result.append(arr[dq[0]])
+                
+        return result
+        
+print(maxOfSubarray(arr = [1, 2, 3, 1, 4, 5, 2, 3, 6], k = 3))
