90eac82ea261263e4d0a5e4798e7c6cfdf037a42
Vishuddh Jain <vishuddh.study@gmail.com>
2026-07-14 16:34:59 +0530

    added another approach for majority element

--- Leetcode/1.Array/3.Majority_Element.py
+++ Leetcode/1.Array/3.Majority_Element.py
@@ -1,15 +1,27 @@
 def majorityElement(nums):
-        dict = {}
-        key, max = 0, 0
-        for i in nums:
-            if i not in dict:
-                dict[i] = 1
-            else:
-                dict[i] += 1
-        for k in dict:
-            val = dict[k]
-            if val >= max:
-                max = val
-                key = k
-        return key
-print(majorityElement([3,2,3]))
+#         dict = {}
+#         key, max = 0, 0
+#         for i in nums:
+#             if i not in dict:
+#                 dict[i] = 1
+#             else:
+#                 dict[i] += 1
+#         for k in dict:
+#             val = dict[k]
+#             if val >= max:
+#                 max = val
+#                 key = k
+#         return key
+# print(majorityElement([3,2,3]))
+    c = 0
+    ele = 0
+    for i in range(len(nums)):
+        if c ==0:
+            ele = nums[i]
+            c+=1
+        elif nums[i] == ele:
+            c+=1
+        else:
+            c-=1
+    return ele
+print(majorityElement([6,5,5]))
