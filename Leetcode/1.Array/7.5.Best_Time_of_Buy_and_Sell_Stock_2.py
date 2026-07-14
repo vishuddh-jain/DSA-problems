bab52a31f7044af927eea716f6467fbd8723132a
Vishuddh Jain <vishuddh.study@gmail.com>
2026-07-14 16:50:44 +0530

    file naem changed

--- Leetcode/1.Array/7.5.Best_Time_of_Buy_and_Sell_Stock_2.py
+++ Leetcode/1.Array/7.5.Best_Time_of_Buy_and_Sell_Stock_2.py
@@ -1,13 +1,14 @@
 def maxProfit(prices):
     i = 0
-    maxp = 0
+    pft = 0
     for j in range(1,len(prices)):
         if prices[j] >= prices[i]:
-            maxp = (prices[j]-prices[i]) + maxp
+            pft = (prices[j]-prices[i]) + pft
             i= j
+            
         else:
             i+=1
-    return maxp
+    return pft
 
 prices = [7,1,5,3,6,4]
 print(maxProfit(prices))
